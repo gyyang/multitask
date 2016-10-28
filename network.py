1ffe0526ab7600c1fef6f3539a9a7fdb4c4fe773
gyyang <gyyang.neuro@gmail.com>
2016-10-28 17:18:42 -0400

    further migration to tensorflow

--- network.py
+++ network.py
@@ -17,82 +17,117 @@ from tensorflow.python.ops import init_ops
 from tensorflow.python.ops import math_ops
 from tensorflow.python.util import nest
 
-from task import *
-
-
 class LeakyRNNCell(RNNCell):
-  """The most basic RNN cell."""
+    """The most basic RNN cell."""
 
-  def __init__(self, num_units, input_size=None, activation=tf.nn.softplus):
-    if input_size is not None:
-      logging.warn("%s: The input_size parameter is deprecated.", self)
-    self._num_units = num_units
-    self._activation = activation
-
-  @property
-  def state_size(self):
-    return self._num_units
-
-  @property
-  def output_size(self):
-    return self._num_units
-
-  def __call__(self, inputs, state, scope=None):
-    """Most basic RNN: output = new_state = activation(W * input + U * state + B)."""
-    with vs.variable_scope(scope or type(self).__name__):  # "BasicRNNCell"
-      output = self._activation(_linear([inputs, state], self._num_units, True))
-    return output, output
+    def __init__(self, num_units, alpha=1.0, input_size=None, activation=tf.nn.softplus):
+        if input_size is not None:
+            logging.warn("%s: The input_size parameter is deprecated.", self)
+        self._num_units = num_units
+        self._activation = activation
+        self._alpha = alpha
 
 
+    @property
+    def state_size(self):
+        return self._num_units
+
+    @property
+    def output_size(self):
+        return self._num_units
+
+    def __call__(self, inputs, state, scope=None):
+        """Leaky RNN: output = new_state = (1-alpha)*state + alpha*activation(W * input + U * state + B)."""
+        with vs.variable_scope(scope or type(self).__name__):    # "LeakyRNNCell"
+            output = (1-self._alpha)*state + \
+                     self._alpha*self._activation(_linear([inputs, state], self._num_units, True))
+        return output, output
 
 def _linear(args, output_size, bias, bias_start=0.0, scope=None):
-  """Linear map: sum_i(args[i] * W[i]), where W[i] is a variable.
+    """Linear map: sum_i(args[i] * W[i]), where W[i] is a variable.
 
-  Args:
-    args: a 2D Tensor or a list of 2D, batch x n, Tensors.
-    output_size: int, second dimension of W[i].
-    bias: boolean, whether to add a bias term or not.
-    bias_start: starting value to initialize the bias; 0 by default.
-    scope: VariableScope for the created subgraph; defaults to "Linear".
+    Args:
+        args: a 2D Tensor or a list of 2D, batch x n, Tensors.
+        output_size: int, second dimension of W[i].
+        bias: boolean, whether to add a bias term or not.
+        bias_start: starting value to initialize the bias; 0 by default.
+        scope: VariableScope for the created subgraph; defaults to "Linear".
 
-  Returns:
-    A 2D Tensor with shape [batch x output_size] equal to
-    sum_i(args[i] * W[i]), where W[i]s are newly created matrices.
+    Returns:
+        A 2D Tensor with shape [batch x output_size] equal to
+        sum_i(args[i] * W[i]), where W[i]s are newly created matrices.
 
-  Raises:
-    ValueError: if some of the arguments has unspecified or wrong shape.
-  """
-  if args is None or (nest.is_sequence(args) and not args):
-    raise ValueError("`args` must be specified")
-  if not nest.is_sequence(args):
-    args = [args]
+    Raises:
+        ValueError: if some of the arguments has unspecified or wrong shape.
+    """
+    if args is None or (nest.is_sequence(args) and not args):
+        raise ValueError("`args` must be specified")
+    if not nest.is_sequence(args):
+        args = [args]
 
-  # Calculate the total size of arguments on dimension 1.
-  total_arg_size = 0
-  shapes = [a.get_shape().as_list() for a in args]
-  for shape in shapes:
-    if len(shape) != 2:
-      raise ValueError("Linear is expecting 2D arguments: %s" % str(shapes))
-    if not shape[1]:
-      raise ValueError("Linear expects shape[1] of arguments: %s" % str(shapes))
-    else:
-      total_arg_size += shape[1]
+    # Calculate the total size of arguments on dimension 1.
+    total_arg_size = 0
+    shapes = [a.get_shape().as_list() for a in args]
+    for shape in shapes:
+        if len(shape) != 2:
+            raise ValueError("Linear is expecting 2D arguments: %s" % str(shapes))
+        if not shape[1]:
+            raise ValueError("Linear expects shape[1] of arguments: %s" % str(shapes))
+        else:
+            total_arg_size += shape[1]
 
-  dtype = [a.dtype for a in args][0]
+    dtype = [a.dtype for a in args][0]
 
-  # Now the computation.
-  with vs.variable_scope(scope or "Linear"):
-    matrix = vs.get_variable(
-        "Matrix", [total_arg_size, output_size], dtype=dtype)
-    if len(args) == 1:
-      res = math_ops.matmul(args[0], matrix)
-    else:
-      res = math_ops.matmul(array_ops.concat(1, args), matrix)
-    if not bias:
-      return res
-    bias_term = vs.get_variable(
-        "Bias", [output_size],
-        dtype=dtype,
-        initializer=init_ops.constant_initializer(
-            bias_start, dtype=dtype))
-  return res + bias_term
+    input_size = total_arg_size - output_size
+    matrix0 = np.random.randn(input_size, output_size)/np.sqrt(input_size)
+    matrix0 = np.concatenate((matrix0, 0.54*np.eye(output_size)), axis=0)
+
+    # Now the computation.
+    with vs.variable_scope(scope or "Linear"):
+        matrix = vs.get_variable(
+                "Matrix", [total_arg_size, output_size], dtype=dtype,
+            initializer=tf.constant_initializer(matrix0, dtype=dtype))
+        if len(args) == 1:
+            res = math_ops.matmul(args[0], matrix)
+        else:
+            res = math_ops.matmul(array_ops.concat(1, args), matrix)
+        if not bias:
+            return res
+        bias_term = vs.get_variable(
+                "Bias", [output_size],
+                dtype=dtype,
+                initializer=init_ops.constant_initializer(
+                        bias_start, dtype=dtype))
+    return res + bias_term
+
+
+def popvec(y):
+    # Population vector read out
+    # Assuming the last dimension is the dimension to be collapsed
+    pref = np.arange(0, 2*np.pi, 2*np.pi/y.shape[-1]) # preferences
+    temp_sum = y.sum(axis=-1)
+    temp_cos = np.sum(y*np.cos(pref), axis=-1)/temp_sum
+    temp_sin = np.sum(y*np.sin(pref), axis=-1)/temp_sum
+    loc = np.arctan2(temp_sin, temp_cos)
+    return np.mod(loc, 2*np.pi)
+
+# performance
+def get_perf(y_hat, y_loc):
+    # Only look at last time points
+    y_loc = y_loc[-1]
+    y_hat = y_hat[-1]
+
+    # Fixation and location of y_hat
+    y_hat_fix = y_hat[...,0]
+    y_hat_loc = popvec(y_hat[...,1:])
+
+    # Fixating? Correctly saccading?
+    corr_fix = y_hat_fix > 0.5
+    corr_loc = abs(y_loc - y_hat_loc)<0.3*np.pi
+
+    # Should fixate?
+    should_fix = y_loc < 0
+
+    # performance
+    perf = should_fix * corr_fix + (1-should_fix) * corr_loc
+    return np.mean(perf)
--- train.py
+++ train.py
@@ -7,6 +7,7 @@ Main training loop and network structure
 from __future__ import division
 
 import os
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
@@ -15,27 +16,38 @@ import tensorflow as tf
 from tensorflow.python.ops import rnn, rnn_cell
 
 from task import *
-from network import LeakyRNNCell
+from network import LeakyRNNCell, get_perf
 
 tf.reset_default_graph()
-HDIM = 50
+
+HDIM = 300
 N_RING = 16
 
-
-config = {'h_type'      : 'leaky_rec_ei',
+config = {'h_type'      : 'leaky_rec',
           'alpha'       : 0.2, # \Delta t/tau
           'dt'          : 0.2*TAU,
           'HDIM'        : HDIM,
           'N_RING'      : N_RING,
           'shape'       : (1+2*N_RING+N_RULE, HDIM, N_RING+1),
-          'save_addon'  : 'tf_'+str(HDIM)}
+          'save_addon'  : 'tf_debug_'+str(HDIM)}
+
+# Rules
+rules = [FIXATION, GO, INHGO, DELAYGO,\
+CHOICE_MOD1, CHOICE_MOD2, CHOICEATTEND_MOD1, CHOICEATTEND_MOD2, CHOICE_INT,\
+CHOICEDELAY_MOD1, CHOICEDELAY_MOD2, CHOICEDELAY_MOD1_COPY,\
+REMAP, INHREMAP, DELAYREMAP,\
+DELAYMATCHGO, DELAYMATCHNOGO, DMCGO, DMCNOGO]
+rule_weights = np.ones(len(rules))
+rule_weights[[rules.index(r) for r in [CHOICEATTEND_MOD1, CHOICEATTEND_MOD2]]] = 10
+rule_weights[[rules.index(r) for r in [DELAYMATCHNOGO, DELAYMATCHGO, DMCNOGO, DMCGO]]] = 5
 
 
 # Parameters
-learning_rate = 0.001
-training_iters = 50000
-batch_size = 10
-display_step = 100
+learning_rate = 0.0001
+training_iters = 1500000
+batch_size_train = 10
+batch_size_test = 200
+display_step = 1000
 
 # Network Parameters
 n_input, n_hidden, n_output = config['shape']
@@ -50,8 +62,7 @@ w_out = tf.Variable(tf.random_normal([n_hidden, n_output]))
 b_out = tf.Variable(tf.random_normal([n_output]))
 
 # Recurrent activity
-rnn_cell = LeakyRNNCell(n_hidden)
-h, states = rnn.dynamic_rnn(rnn_cell, x, dtype=tf.float32)
+h, states = rnn.dynamic_rnn(LeakyRNNCell(n_hidden), x, dtype=tf.float32)
 
 # Output
 y_hat = tf.sigmoid(tf.matmul(tf.reshape(h, (-1, n_hidden)), w_out) + b_out)
@@ -63,22 +74,62 @@ optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 # Initializing the variables
 init = tf.initialize_all_variables()
 
+# Add ops to save and restore all the variables.
+saver = tf.train.Saver()
+
+# Store results
+trials     = []
+times      = []
+cost_tests = {rule:[] for rule in rules}
+perf_tests = {rule:[] for rule in rules}
+
 # Launch the graph
+t_start = time.time()
 with tf.Session() as sess:
     sess.run(init)
     step = 1
     # Keep training until reach max iterations
-    while step * batch_size < training_iters:
-        task = generate_onebatch(GO, config, 'random', batch_size=batch_size)
-        sess.run(optimizer, feed_dict={x: task.x,
-                                       y: task.y.reshape((-1,n_output)),
-                                       c_mask: task.c_mask.reshape((-1,n_output))})
-        if step % display_step == 0:
-            # Calculate batch accuracy
-            c_sample = sess.run(cost, feed_dict={x: task.x,
-                                       y: task.y.reshape((-1,n_output)),
-                                       c_mask: task.c_mask.reshape((-1,n_output))})
-            print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
-                  "{:.6f}".format(c_sample))
-        step += 1
+    while step * batch_size_train < training_iters:
+        try:
+            # Training
+            rule = np.random.choice(rules, p=rule_weights/rule_weights.sum())
+            task = generate_onebatch(rule, config, 'random', batch_size=batch_size_train)
+            sess.run(optimizer, feed_dict={x: task.x,
+                                           y: task.y.reshape((-1,n_output)),
+                                           c_mask: task.c_mask.reshape((-1,n_output))})
+
+            # Validation
+            if step % display_step == 0:
+                trials.append(step*batch_size_train)
+                times.append(time.time()-t_start)
+                print('Trial {:7d}'.format(trials[-1]) +
+                      '  | Time {:0.2f} s'.format(times[-1]))
+                # Calculate batch accuracy
+                c_tests = list()
+                for rule in rules:
+                    task = generate_onebatch(rule, config, 'random', batch_size=batch_size_test)
+                    y_hat_test = sess.run(y_hat, feed_dict={x: task.x})
+                    y_hat_test = y_hat_test.reshape((-1,batch_size_test,n_output))
+                    c_test = np.mean(task.c_mask*(y_hat_test-task.y)**2)
+                    perf_test = get_perf(y_hat_test, task.y_loc)
+                    cost_tests[rule].append(c_test)
+                    perf_tests[rule].append(perf_test)
+                    print('{:15s}'.format(rule_name[rule]) +
+                          '| cost {:0.2f}'.format(c_test)  +
+                          '| perf {:0.2f}'.format(perf_test))
+            step += 1
+        except KeyboardInterrupt:
+            break
+
+    # Saving the model
+    save_path = saver.save(sess, os.path.join('data', config['save_addon']+'.ckpt'))
+    print("Model saved in file: %s" % save_path)
+
+    config['trials']     = trials
+    config['times']      = times
+    config['cost_tests'] = cost_tests
+    config['perf_tests'] = perf_tests
+    with open('data/config'+config['save_addon']+'.pkl', 'wb') as f:
+        pickle.dump(config, f)
+
     print("Optimization Finished!")
