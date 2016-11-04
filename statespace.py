75562606bfa6ce50b6d2709d2c05b0487073bdb2
gyyang <gyyang.neuro@gmail.com>
2016-11-04 18:49:48 -0400

    using one location

--- statespace.py
+++ statespace.py
@@ -23,94 +23,141 @@ mpl.rcParams['xtick.direction'] = 'out'
 mpl.rcParams['ytick.direction'] = 'out'
 
 
-# save_addon = 'tf_latest_500'
-# rules = [CHOICEATTEND_MOD1, CHOICEATTEND_MOD2, CHOICE_INT]
-#
-#
-# print('Starting standard analysis of the CHOICEATTEND task...')
-# with Run(save_addon) as R:
-#
-#     n_tar_loc = 100 # increase repeat by increasing this
-#     n_tar = 2
-#     batch_size = n_tar_loc * n_tar**2
-#     batch_shape = (n_tar_loc,n_tar,n_tar)
-#     ind_tar_loc, ind_tar_mod1, ind_tar_mod2 = np.unravel_index(range(batch_size),batch_shape)
-#
-#     tar1_locs = 2*np.pi*ind_tar_loc/n_tar_loc
-#     tar2_locs = (tar1_locs+np.pi)%(2*np.pi)
-#
-#     tar_str_range = 0.4
-#     tar1_mod1_strengths = (1-tar_str_range/2)+tar_str_range*ind_tar_mod1/(n_tar-1)
-#     tar2_mod1_strengths = 2 - tar1_mod1_strengths
-#     tar1_mod2_strengths = (1-tar_str_range/2)+tar_str_range*ind_tar_mod2/(n_tar-1)
-#     tar2_mod2_strengths = 2 - tar1_mod2_strengths
-#
-#     params = {'tar1_locs' : tar1_locs,
-#               'tar2_locs' : tar2_locs,
-#               'tar1_mod1_strengths' : tar1_mod1_strengths,
-#               'tar2_mod1_strengths' : tar2_mod1_strengths,
-#               'tar1_mod2_strengths' : tar1_mod2_strengths,
-#               'tar2_mod2_strengths' : tar2_mod2_strengths,
-#               'tar_time'    : 800}
-#
-#     h_samples = dict()
-#     for rule in rules:
-#         task  = generate_onebatch(rule, R.config, 'psychometric', params=params)
-#         # Only study target epoch
-#         epoch = task.epochs['tar1']
-#         h_samples[rule] = R.f_h(task.x)[epoch[0]:epoch[1],...][::20,...]
-#
-#     for rule in [DMCGO]: # for control
-#         task  = generate_onebatch(rule, R.config, 'test')
-#         h_samples[rule] = R.f_h(task.x)[::50,...]
-#
-# nt, nb, nh = h_samples[CHOICEATTEND_MOD1].shape
-# from sklearn.decomposition import PCA
-# pca = PCA()
-# pca.fit(h_samples[CHOICEATTEND_MOD1].reshape((-1, nh)))
-#
-# h_trans = dict()
-# for rule in rules:
-#     h_trans[rule] = pca.transform(h_samples[rule].reshape((-1, nh)))
-# ev = pca.explained_variance_
-# evr = pca.explained_variance_ratio_
-# # plt.figure()
-# # plt.plot(h_trans[:,0], h_trans[:,1], '.')
-# # plt.show()
-#
-# # Plot multiple rules:
-# # colors = sns.color_palette('dark', len(rules))
-# # fig = plt.figure()
-# # ax = fig.add_subplot(111, projection='3d')
-# # for i, rule in enumerate(rules):
-# #     h_plot = h_trans[rule]
-# #     h_plot = h_plot.reshape((nt, nb, nh))
-# #     #h_plot = h_plot[:,tar1_locs==np.unique(tar1_locs)[1],:]
-# #     h_plot = h_plot.reshape((-1,nh))
-# #     ax.plot(h_plot[:,0], h_plot[:,1], h_plot[:,2], '.', markersize=2, color=colors[i])
-# #     ax.plot([h_plot[0,0]], [h_plot[0,1]], [h_plot[0,2]], 'd', markersize=5, color=colors[i])
-# # plt.show()
-#
-# # Plot multiple conditions
-# rule = CHOICEATTEND_MOD1
-# task  = generate_onebatch(rule, R.config, 'psychometric', params=params)
-# y_locs = task.y_loc[-1]
-#
-# #separate_bys = tar1_locs
-# #separate_bys = tar1_mod1_strengths
-# separate_bys = y_locs
-#
-# #colors = sns.color_palette('dark', len(np.unique(separate_bys)))
-# colors = sns.color_palette("husl", len(np.unique(separate_bys)))
-# h_tran = h_trans[rule].reshape((nt, nb, nh))
+save_addon = 'tf_latest_500'
+rules = [CHOICEATTEND_MOD1, CHOICEATTEND_MOD2, CHOICE_INT]
+
+sub_mean = True # Not clear if legit yet
+def f_sub_mean(x):
+    # subtract mean activity across batch conditions
+    assert(len(x.shape)==3)
+    x_mean = x.mean(axis=1)
+    for i in range(x.shape[1]):
+        x[:,i,:] -= x_mean
+    return x
+
+print('Starting standard analysis of the CHOICEATTEND task...')
+with Run(save_addon) as R:
+
+    n_tar = 6
+    batch_size = n_tar**2
+    batch_shape = (n_tar,n_tar)
+    ind_tar_mod1, ind_tar_mod2 = np.unravel_index(range(batch_size),batch_shape)
+
+    tar1_loc  = 0
+    tar1_locs = np.ones(batch_size)*tar1_loc
+    tar2_locs = (tar1_locs+np.pi)%(2*np.pi)
+
+    tar_str_range = 0.2
+    tar1_mod1_strengths = (1-tar_str_range/2)+tar_str_range*ind_tar_mod1/(n_tar-1)
+    tar2_mod1_strengths = 2 - tar1_mod1_strengths
+    tar1_mod2_strengths = (1-tar_str_range/2)+tar_str_range*ind_tar_mod2/(n_tar-1)
+    tar2_mod2_strengths = 2 - tar1_mod2_strengths
+
+    params = {'tar1_locs' : tar1_locs,
+              'tar2_locs' : tar2_locs,
+              'tar1_mod1_strengths' : tar1_mod1_strengths,
+              'tar2_mod1_strengths' : tar2_mod1_strengths,
+              'tar1_mod2_strengths' : tar1_mod2_strengths,
+              'tar2_mod2_strengths' : tar2_mod2_strengths,
+              'tar_time'    : 800}
+
+    h_samples = dict()
+    for rule in rules:
+        task  = generate_onebatch(rule, R.config, 'psychometric', params=params)
+        # Only study target epoch
+        epoch = task.epochs['tar1']
+        h_sample = R.f_h(task.x)[epoch[0]:epoch[1],...][::20,...]
+        if sub_mean:
+            h_sample = f_sub_mean(h_sample)
+        h_samples[rule] = h_sample
+
+nt, nb, nh = h_samples[CHOICEATTEND_MOD1].shape
+from sklearn.decomposition import PCA
+pca = PCA()
+pca.fit(h_samples[CHOICEATTEND_MOD1].reshape((-1, nh)))
+
+h_trans = dict()
+for rule in rules:
+    h_trans[rule] = pca.transform(h_samples[rule].reshape((-1, nh)))
+ev = pca.explained_variance_
+evr = pca.explained_variance_ratio_
+# plt.figure()
+# plt.plot(h_trans[:,0], h_trans[:,1], '.')
+# plt.show()
+
+# Plot multiple rules:
+# colors = sns.color_palette('dark', len(rules))
 # fig = plt.figure()
 # ax = fig.add_subplot(111, projection='3d')
-# for i, s in enumerate(np.unique(separate_bys)):
-#     h_plot = h_tran[:,separate_bys==s,:].reshape((-1,nh))
+# for i, rule in enumerate(rules):
+#     h_plot = h_trans[rule]
+#     h_plot = h_plot.reshape((nt, nb, nh))
+#     #h_plot = h_plot[:,tar1_locs==np.unique(tar1_locs)[1],:]
+#     h_plot = h_plot.reshape((-1,nh))
 #     ax.plot(h_plot[:,0], h_plot[:,1], h_plot[:,2], '.', markersize=2, color=colors[i])
 #     ax.plot([h_plot[0,0]], [h_plot[0,1]], [h_plot[0,2]], 'd', markersize=5, color=colors[i])
 # plt.show()
-#
+
+# Plot multiple conditions
+rule = CHOICEATTEND_MOD1
+task  = generate_onebatch(rule, R.config, 'psychometric', params=params)
+y_locs = task.y_loc[-1]
+
+#separate_bys = tar1_locs
+
+separate_bys = tar1_mod1_strengths
+#separate_bys = tar1_mod2_strengths
+
+#separate_bys = y_locs
+#separate_bys = np.arange(nb)
+
+#colors = sns.color_palette('dark', len(np.unique(separate_bys)))
+#colors = sns.color_palette("husl", len(np.unique(separate_bys)))
+colors = sns.color_palette("RdBu_r", len(np.unique(separate_bys)))
+h_tran = h_trans[rule].reshape((nt, nb, -1))
+
+
+#==============================================================================
+# pcs = [0,1]
+# fig = plt.figure(figsize=(4,4))
+# ax = fig.add_axes([.2,.2,.7,.7])    
+# for i, s in enumerate(np.unique(separate_bys)):
+#     h_plot = h_tran[:,separate_bys==s,:]
+#     for j in range(h_plot.shape[1]):
+#         ax.plot(h_plot[:,j,pcs[0]], h_plot[:,j,pcs[1]], '.-', markersize=2, color=colors[i])
+#         ax.plot([h_plot[0,j,pcs[0]]], [h_plot[0,j,pcs[1]]], 'd', markersize=5, color=colors[i])
+# plt.savefig('figure/temp.pdf')
+# plt.show()
+# 
+# 
+# 
+# colors = sns.color_palette("husl", nb)
+# pcs = [0,1]
+# fig = plt.figure(figsize=(4,4))
+# ax = fig.add_axes([.2,.2,.7,.7])
+# for i in range(nb):
+#     h_plot = h_tran[:,i,:]
+#     ax.plot(h_plot[:,pcs[0]], h_plot[:,pcs[1]], '.-', markersize=2, color=colors[i])
+#     ax.plot([h_plot[0,pcs[0]]], [h_plot[0,pcs[1]]], 'd', markersize=5, color=colors[i])
+#     plt.text(h_plot[-1,pcs[0]], h_plot[-1,pcs[1]],
+#              '{:0.1f},{:0.1f},{:0.1f},{:0.1f}'.format(tar1_locs[i], y_locs[i], tar1_mod1_strengths[i], tar1_mod2_strengths[i]),
+#              fontsize=7)
+# plt.savefig('figure/temp.pdf')
+# plt.show()
+#==============================================================================
+
+def show3D():
+    pcs = [0,1,2]
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    for i, s in enumerate(np.unique(separate_bys)):
+        h_plot = h_tran[:,separate_bys==s,:]
+        for j in range(h_plot.shape[1]):
+            ax.plot(h_plot[:,j,pcs[0]], h_plot[:,j,pcs[1]], h_plot[:,j,pcs[2]],
+                    '.-', markersize=2, color=colors[i])
+    ax.azim = -62
+    ax.elev = 62
+    plt.show()
 
 
 def test_PCA_ring():
