c92ace6736115deb35ef97ce581f4379fa19d592
gyyang <gyyang.neuro@gmail.com>
2016-11-07 16:32:26 -0500

    initial commit for remap analysis

---
+++ remap.py
@@ -0,0 +1,187 @@
+"""
+Analysis of remap units
+"""
+
+from __future__ import division
+
+import os
+import numpy as np
+import pickle
+import time
+import copy
+from collections import OrderedDict
+import scipy.stats as stats
+from scipy.optimize import curve_fit, minimize
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import seaborn.apionly as sns # If you don't have this, then some colormaps won't work
+from task import *
+from run import Run
+from network import get_perf
+
+save_addon = 'tf_latest_400'
+
+
+###################### Plot single unit activity ##########################
+HDIM = 202
+N_RING = 16
+save_addon = 'chanceabbott_'+str(HDIM)+'_'+str(N_RING)
+A = SumStatAnalysis('rule', save_addon)
+inds_remap = A.find_remap_units()
+
+# A.plot_unit_connection(inds)
+ga = GeneralAnalysis(save_addon=A.save_addon_original)
+success = False
+for ind in inds_remap:
+    res = linregress(ga.Wout[:,ind][1:],ga.Win[ind,1:1+N_RING])
+    if res.rvalue<-0.9 and res.pvalue<0.01:
+        success = True
+        break
+
+if success:
+    rules = [INHGO, DELAYREMAP, INHREMAP]
+    ga.run_test(rules=rules)
+    ga.plot_singleneuron_intime(neuron_id=ind, rules=rules, save_fig=True)
+else:
+    raise ValueError('Did not success to find a typical remap unit in this network')
+########### Plot Causal Manipulation Results  #############################
+
+perfs = ga.get_performances()
+
+# inh_id = ind
+inh_id = inds_remap
+ga_inh = GeneralAnalysis(save_addon=save_addon, inh_id=inh_id, inh_output=True)
+perfs_inh = ga_inh.get_performances()
+
+fig = plt.figure(figsize=(2.5,2))
+ax = fig.add_axes([0.2,0.5,0.75,0.4])
+ax.plot(perfs, 'o-', markersize=3, label='intact', color=sns.xkcd_palette(['black'])[0])
+ax.plot(perfs_inh, 'o-', markersize=3, label='Inh. unit \n'+str(inh_id), color=sns.xkcd_palette(['red'])[0])
+plt.xticks(range(len(ga.rules)), [rule_name[rule] for rule in ga.rules], rotation=90)
+plt.ylabel('performance', fontsize=7)
+plt.ylim(bottom=0.0, top=1.05)
+plt.xlim([-0.5, len(perfs)-0.5])
+ax.tick_params(axis='both', which='major', labelsize=7)
+leg = plt.legend(fontsize=6,frameon=False, loc=2, numpoints=1,
+                 bbox_to_anchor=(0,0.5), labelspacing=0.3)
+ax.spines["right"].set_visible(False)
+ax.spines["top"].set_visible(False)
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+plt.locator_params(axis='y', nbins=4)
+plt.savefig('figure/performance_inh'+str(inh_id)+A.config['save_addon']+'.pdf', transparent=True)
+plt.show()
+
+###################### Plot single unit connection ##########################
+# Connection with ring input and output
+w_in = ga.Win[ind,1:1+N_RING]
+w_out = ga.Wout[:,ind][1:]
+fig = plt.figure(figsize=(1.5,1.5))
+ax = fig.add_axes([0.3,0.3,0.6,0.5])
+ax.plot(w_in, color=sns.xkcd_palette(['green'])[0], label='from input')
+ax.plot(w_out, color=sns.xkcd_palette(['blue'])[0], label='to output')
+plt.ylabel('conn. weight', fontsize=7, labelpad=1)
+plt.xlabel('ring', fontsize=7)
+plt.xticks([0,(N_RING-1)/2,N_RING-1],[r'0$\degree$',r'180$\degree$',r'360$\degree$'])
+plt.title('Unit {:d} '.format(ind), fontsize=7)
+
+wmax = max((w_in.max(),w_out.max()))
+wmin = min((w_in.min(),w_out.min()))
+plt.ylim([wmin-0.1*(wmax-wmin),wmax+0.7*(wmax-wmin)])
+ax.tick_params(axis='both', which='major', labelsize=7)
+ax.spines["right"].set_visible(False)
+ax.spines["top"].set_visible(False)
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+plt.locator_params(axis='y',nbins=5)
+lg = plt.legend(ncol=1,bbox_to_anchor=(1,1.1),frameon=False,
+                fontsize=7,labelspacing=0.3,loc=1)
+plt.savefig('figure/sample_remap_unit_connectivity.pdf', transparent=True)
+plt.show()
+
+
+########### Plot histogram of correlation coefficient #####################
+slopes = list()
+rvalues = list()
+pvalues = list()
+remap_units = list()
+HDIMs = range(190,207)
+for j, HDIM in enumerate(HDIMs):
+    N_RING = 16
+    save_addon = 'chanceabbott_'+str(HDIM)+'_'+str(N_RING)
+    A = SumStatAnalysis('rule', save_addon)
+    inds_remap = A.find_remap_units()
+
+    # A.plot_unit_connection(inds)
+    ga = GeneralAnalysis(save_addon=A.save_addon_original)
+    for ind in range(HDIM):
+        res = linregress(ga.Wout[:,ind][1:],ga.Win[ind,1:1+N_RING])
+        slopes.append(res.slope)
+        rvalues.append(res.rvalue)
+        pvalues.append(res.pvalue)
+        remap_units.append(ind in inds_remap)
+
+    if j == 0:
+        conn_rule_to_all = ga.Win[:, 1+2*N_RING:] # connection from rule inputs to all units
+        conn_rule_to_remap = ga.Win[inds_remap, 1+2*N_RING:] # connection from rule inputs to remap units
+    else:
+        conn_rule_to_all = np.concatenate((conn_rule_to_all, ga.Win[:, 1+2*N_RING:]))
+        conn_rule_to_remap = np.concatenate((conn_rule_to_remap, ga.Win[inds_remap, 1+2*N_RING:]))
+
+
+slopes, rvalues, pvalues, remap_units = np.array(slopes), np.array(rvalues), np.array(pvalues), np.array(remap_units)
+
+# plot_value, plot_range = slopes, (-4,4)
+plot_value, plot_range = rvalues, (-1,1)
+thres = 0.01 ##TODO: Find out how to set this threshold
+for i in [0,1]:
+    if i == 0:
+        units = remap_units
+        title = 'Remap'
+    else:
+        units = (1-remap_units).astype(bool)
+        title = 'Non-remap'
+
+    fig = plt.figure(figsize=(1.5,1.2))
+    ax = fig.add_axes([0.3,0.3,0.6,0.5])
+    hist, bins_edge = np.histogram(plot_value[units], range=plot_range, bins=30)
+    ax.bar(bins_edge[:-1], hist, width=bins_edge[1]-bins_edge[0], color=sns.xkcd_palette(['navy blue'])[0], edgecolor='none')
+    hist, bins_edge = np.histogram(plot_value[units*(pvalues<thres)], range=plot_range, bins=30)
+    ax.bar(bins_edge[:-1], hist, width=bins_edge[1]-bins_edge[0], color=sns.xkcd_palette(['cerulean'])[0], edgecolor='none')
+    plt.xlabel('corr. coeff.', fontsize=7, labelpad=1)
+    plt.ylabel('counts', fontsize=7)
+    plt.locator_params(nbins=3)
+    plt.title(title+' units ({:d} nets)'.format(len(HDIMs)) , fontsize=7)
+    ax.tick_params(axis='both', which='major', labelsize=7)
+    ax.spines["right"].set_visible(False)
+    ax.spines["top"].set_visible(False)
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+    plt.savefig('figure/'+title+'_unit_cc.pdf', transparent=True)
+    plt.show()
+
+########### Plot connections averaged across networks #####################
+fig = plt.figure(figsize=(2.5,2))
+ax = fig.add_axes([0.2,0.5,0.75,0.4])
+ax.plot(conn_rule_to_all.mean(axis=0)[ga.rules], 'o-', markersize=3, label='all', color=sns.xkcd_palette(['black'])[0])
+ax.plot(conn_rule_to_remap.mean(axis=0)[ga.rules], 'o-', markersize=3, label='remap', color=sns.xkcd_palette(['red'])[0])
+
+plt.xticks(range(len(ga.rules)), [rule_name[rule] for rule in ga.rules], rotation=90)
+plt.ylabel('Mean conn. from rule', fontsize=7)
+plt.title('Average of {:d} networks'.format(len(HDIMs)), fontsize=7)
+#plt.ylim(bottom=0.0, top=1.05)
+plt.xlim([-0.5, len(ga.rules)-0.5])
+ax.tick_params(axis='both', which='major', labelsize=7)
+leg = plt.legend(title='to units',fontsize=7,frameon=False, loc=2, numpoints=1,
+                 bbox_to_anchor=(0.3,0.6), labelspacing=0.3)
+plt.setp(leg.get_title(),fontsize=7)
+ax.spines["right"].set_visible(False)
+ax.spines["top"].set_visible(False)
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+plt.locator_params(axis='y', nbins=4)
+plt.savefig('figure/conn_ruleinput_to_remap.pdf', transparent=True)
+plt.show()
+
+
