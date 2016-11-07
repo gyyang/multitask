7ccebb27ced37af2f7dbc5ca0542710cf5d764e1
gyyang <gyyang.neuro@gmail.com>
2016-11-07 16:08:58 -0500

    deleted 3D plot

--- statespace.py
+++ statespace.py
@@ -49,7 +49,7 @@ print('Starting standard analysis of the CHOICEATTEND task...')
 with Run(save_addon) as R:
 
     n_tar = 6
-    n_rep = 30
+    n_rep = 10
     batch_size = n_rep * n_tar**2
     batch_shape = (n_rep, n_tar, n_tar)
     ind_rep, ind_tar_mod1, ind_tar_mod2 = np.unravel_index(range(batch_size),batch_shape)
@@ -115,88 +115,6 @@ if z_score:
     h = h/h.std(axis=0)
 
 h = h.reshape((nt, nb, h.shape[-1]))
-
-def show3D(h_tran, separate_by, pcs=(0,1,2), **kwargs):
-    if separate_by == 'tar1_mod1_strengths':
-        separate_bys = tar1_mod1_strengths
-        colors = sns.color_palette("RdBu_r", len(np.unique(separate_bys)))
-    elif separate_by == 'tar1_mod2_strengths':
-        separate_bys = tar1_mod2_strengths
-        colors = sns.color_palette("BrBG", len(np.unique(separate_bys)))
-    else:
-        raise ValueError
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    for i, s in enumerate(np.unique(separate_bys)):
-        h_plot = h_tran[:,separate_bys==s,:]
-        for j in range(h_plot.shape[1]):
-            ax.plot(h_plot[:,j,pcs[0]], h_plot[:,j,pcs[1]], h_plot[:,j,pcs[2]],
-                    '.-', markersize=2, color=colors[i])
-    if 'azim' in kwargs:
-        ax.azim = kwargs['azim']
-    if 'elev' in kwargs:
-        ax.elev = kwargs['elev']
-    ax.elev = 62
-    plt.show()
-
-def test_PCA_ring():
-    # Test PCA with simple ring representation
-    n_t = 3
-    n_loc = 256
-    n_ring = 64
-    h_simple = np.zeros((n_t, n_loc, n_ring))
-
-    pref = np.arange(0,2*np.pi,2*np.pi/n_ring) # preferences
-    locs = np.arange(n_loc)/n_loc*2*np.pi
-    ts   = np.arange(n_t)/n_t
-    for i, loc in enumerate(locs):
-        dist = get_dist(loc-pref) # periodic boundary
-        dist /= np.pi/8
-        h_simple[:,i,:] = 0.8*np.exp(-dist**2/2)
-        h_simple[:,i,:] = (h_simple[:,i,:].T * ts).T
-
-    from sklearn.decomposition import PCA
-    pca = PCA()
-    h_tran = pca.fit_transform(h_simple.reshape((-1, n_ring))).reshape((n_t, n_loc, n_ring))
-    ev = pca.explained_variance_
-    evr = pca.explained_variance_ratio_
-    colors = sns.color_palette("husl", n_loc)
-
-    h_tran += np.random.randn(*h_tran.shape)*0.004
-
-    fig = plt.figure(figsize=(4,4))
-    ax = fig.add_axes([.2,.2,.7,.7])
-    for i in range(n_loc):
-        ax.plot(h_tran[:,i,0], h_tran[:,i,1], '-', linewidth=0.3, color=colors[i])
-    ax.set_aspect('equal')
-    plt.savefig('figure/temp.pdf')
-    plt.show()
-
-    fig = plt.figure(figsize=(4,4))
-    ax = fig.add_axes([.2,.2,.7,.7])
-    ax.plot(evr, 'o-')
-    plt.show()
-
-
-# nt, nb, nh = h_samples[CHOICEATTEND_MOD1].shape
-# from sklearn.decomposition import PCA
-# pca = PCA()
-# #pca.fit(h_samples[CHOICEATTEND_MOD1].reshape((-1, nh)))
-# pca.fit(np.concatenate((h_samples[CHOICEATTEND_MOD1].reshape((-1, nh)),h_samples[CHOICEATTEND_MOD2].reshape((-1, nh))), axis=0))
-#
-# h_trans = dict()
-# for rule in rules:
-#     h_trans[rule] = pca.transform(h_samples[rule].reshape((-1, nh)))
-
-#h_tran = h_trans[CHOICEATTEND_MOD1].reshape((nt, nb, -1))
-#show3D(h_tran, 'tar1_mod1_strengths', azim=-62, elev=62)
-#show3D(h_tran, 'tar1_mod2_strengths', azim=-62, elev=62)
-
-#h_tran = h_trans[CHOICEATTEND_MOD2].reshape((nt, nb, -1))
-#show3D(h_tran, 'tar1_mod1_strengths', azim=-62, elev=62)
-#show3D(h_tran, 'tar1_mod2_strengths', azim=-62, elev=62)
-
 
 
 # Regression
