adabb4b3dcc5a341876e533f61f7973b4dc5d514
gyyang <gyyang.neuro@gmail.com>
2016-12-30 11:20:33 -0500

    added big plot of all rule pairs

--- variance.py
+++ variance.py
@@ -77,25 +77,126 @@ def compute_variance(save_addon, data_type, rules,
         pickle.dump(result, f)
 
 
-def plot_hist_totalvar(save_addon, data_type):
-    with open('data/variance'+data_type+save_addon+'.pkl','rb') as f:
+def plot_hist_varprop(save_addon, rules):
+    ## TODO: Finish this. This is exciting!
+    '''
+    Plot histogram of proportion of variance for some tasks across units
+    :param save_addon:
+    :param data_type:
+    :param rules: list of rules. Show proportion of variance for the first rule
+    :return:
+    '''
+    data_type = 'rule'
+    assert len(rules) == 2
+    assert data_type == 'rule'
+    # If not computed, use variance.py
+    # fname = 'data/variance'+data_type+save_addon+'_rr'
+    fname = 'data/variance'+data_type+save_addon
+    with open(fname+'.pkl','rb') as f:
         res = pickle.load(f)
-    # Plot total variance distribution
+    h_var_all = res['h_var_all']
+    keys      = res['keys']
+
+    ind_rules = [keys.index(rule) for rule in rules]
+    h_var_all = h_var_all[:, ind_rules]
+
+    # First only get active units. Total variance across tasks larger than 1e-3
+    ind_active = np.where(h_var_all.sum(axis=1) > 1e-3)[0]
+    h_var_all  = h_var_all[ind_active, :]
+
+    # Normalize by the total variance across tasks
+    h_normvar_all = (h_var_all.T/np.sum(h_var_all, axis=1)).T
+
+    # Plot the proportion of variance for the first rule
+    data_plot = h_normvar_all[:, 0]
+
+    fs = 6
     fig = plt.figure(figsize=(1.5,1.2))
     ax = fig.add_axes([0.3,0.3,0.6,0.5])
-    hist, bins_edge = np.histogram(np.log10(res['h_var_all'].sum(axis=1)), bins=30)
+    hist, bins_edge = np.histogram(data_plot, bins=30)
     ax.bar(bins_edge[:-1], hist, width=bins_edge[1]-bins_edge[0],
            color=sns.xkcd_palette(['cerulean'])[0], edgecolor='none')
-    plt.xlabel(r'$log_{10}$ total variance', fontsize=7, labelpad=1)
-    plt.ylabel('counts', fontsize=7)
     plt.locator_params(nbins=3)
-    ax.tick_params(axis='both', which='major', labelsize=7)
-    ax.spines["right"].set_visible(False)
-    ax.spines["top"].set_visible(False)
-    ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_ticks_position('left')
-    # plt.savefig('figure/hist_totalvar.pdf', transparent=True)
-    plt.show()
+    # xlabel = r'$\frac{Var({:s})}{[Var({:s})+Var({:s})]}$'.format(rule_name[rules[0]],rule_name[rules[0]],rule_name[rules[1]])
+    xlabel = 'VarRatio({:s},{:s})'.format(rule_name[rules[0]], rule_name[rules[1]])
+    ax.set_xlabel(xlabel, fontsize=fs)
+    ax.set_ylim(bottom=-1)
+    ax.set_xlim([-0.1,1.1])
+    ax.tick_params(axis='both', which='major', labelsize=fs, length=2)
+
+
+def plot_hist_varprop_all(save_addon):
+    ## TODO: Finish this. This is exciting!
+    '''
+    Plot histogram of proportion of variance for some tasks across units
+    :param save_addon:
+    :param data_type:
+    :param rules: list of rules. Show proportion of variance for the first rule
+    :return:
+    '''
+    data_type = 'rule'
+    rules = [GO, INHGO, DELAYGO,\
+        CHOICE_MOD1, CHOICE_MOD2, CHOICEATTEND_MOD1, CHOICEATTEND_MOD2, CHOICE_INT,\
+        CHOICEDELAY_MOD1, CHOICEDELAY_MOD2,\
+        REMAP, INHREMAP, DELAYREMAP,\
+        DELAYMATCHGO, DELAYMATCHNOGO, DMCGO, DMCNOGO]
+    # rules = [GO, INHGO, DELAYGO]
+
+    assert data_type == 'rule'
+    # If not computed, use variance.py
+    # fname = 'data/variance'+data_type+save_addon+'_rr'
+    fname = 'data/variance'+data_type+save_addon
+    with open(fname+'.pkl','rb') as f:
+        res = pickle.load(f)
+    h_var_all = res['h_var_all']
+    keys      = res['keys']
+
+    f, axarr = plt.subplots(len(rules), len(rules), figsize=(8, 8))
+    for i in range(len(rules)):
+        for j in range(len(rules)):
+            if j>=i:
+                axarr[i,j].axis('off')
+                continue
+
+            rules_tmp = [rules[i], rules[j]]
+            ind_rules = [keys.index(rule) for rule in rules_tmp]
+            h_var_all_tmp = h_var_all[:, ind_rules]
+
+            # First only get active units. Total variance across tasks larger than 1e-3
+            ind_active = np.where(h_var_all.sum(axis=1) > 1e-3)[0]
+            h_var_all_tmp  = h_var_all_tmp[ind_active, :]
+
+            # Normalize by the total variance across tasks
+            h_normvar_all = (h_var_all_tmp.T/np.sum(h_var_all_tmp, axis=1)).T
+
+            # Plot the proportion of variance for the first rule
+            data_plot = h_normvar_all[:, 1]
+
+            fs = 5
+            ax = axarr[i, j]
+            hist, bins_edge = np.histogram(data_plot, bins=25, range=(0,1))
+            ax.bar(bins_edge[:-1], hist, width=bins_edge[1]-bins_edge[0],
+                   color=sns.xkcd_palette(['cerulean'])[0], edgecolor='none')
+            plt.locator_params(nbins=3)
+            # xlabel = r'$\frac{Var({:s})}{[Var({:s})+Var({:s})]}$'.format(rule_name[rules[0]],rule_name[rules[0]],rule_name[rules[1]])
+            xlabel = 'VarRatio({:s},{:s})'.format(rule_name[rules[0]], rule_name[rules[1]])
+            # ax.set_xlabel(xlabel, fontsize=fs)
+            ax.set_ylim(bottom=-1)
+            ax.set_xticks([0,1])
+            ax.set_xticklabels([])
+            ax.set_yticks([])
+            ax.set_xlim([-0.1,1.1])
+            ax.xaxis.set_ticks_position('bottom')
+            ax.tick_params(axis='both', which='major', labelsize=fs, length=2)
+            ax.spines["right"].set_visible(False)
+            ax.spines["left"].set_visible(False)
+            ax.spines["top"].set_visible(False)
+            if i == len(rules)-1:
+                ax.set_xlabel(rule_name[rules[j]], fontsize=fs)
+            if j == 0:
+                ax.set_ylabel(rule_name[rules[i]], fontsize=fs)
+
+    plt.savefig('figure/temp.pdf', transparent=True)
 
 
 
@@ -158,8 +259,9 @@ if __name__ == '__main__':
     # compute_variance(save_addon, data_type, rules, random_rotation=True)
     # plot_hist_totalvar(save_addon, data_type)
     # print('Time taken {:0.2f} s'.format(time.time()-start))
-    get_random_rotation_variance(save_addon, data_type)
-
+    # get_random_rotation_variance(save_addon, data_type)
+    # plot_hist_varprop(save_addon='choiceonly_weaknoise_300', rules=[CHOICE_MOD1, CHOICE_MOD2])
+    plot_hist_varprop_all('allrule_weaknoise_300')
 
     # rule_hist = CHOICEATTEND_MOD1
     # fig = plt.figure(figsize=(1.5,1.2))
