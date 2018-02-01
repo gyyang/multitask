bb20728787bf18bfea48c02dc53712da729fc4bb
madhurajoglekar <countess.steph@gmail.com>
2018-02-01 10:14:49 -0500

    checkperf

--- varyhparams.py
+++ varyhparams.py
@@ -3,11 +3,11 @@
 from __future__ import division
 
 import os
-import numpy as np
+#import numpy as np
 import matplotlib.pyplot as plt
 
 import tools
-import variance
+#import variance
 import clustering
 
 DATAPATH = os.path.join(os.getcwd(), 'data', 'debug')
@@ -16,15 +16,32 @@ DATAPATH = os.path.join(os.getcwd(), 'data', 'debug')
 train_dirs = [os.path.join(DATAPATH, d) for d in os.listdir(DATAPATH)]
 train_dirs = [d for d in train_dirs if os.path.isdir(d)]
 
+from os import listdir
+#check if training is completed 
+from os.path import isfile, join
+train_dirs = [d for d in train_dirs if 'variance_rule.pkl' in [f for f in listdir(d) if isfile(join(d, f))]]#maddy added
+
+
 # Compute the number of clusters
 n_clusters = list()
 hparams_list = list()
 for train_dir in train_dirs:
     hparams = tools.load_hparams(train_dir)
-    # variance.compute_variance(train_dir)
-    analysis = clustering.Analysis(train_dir, 'rule')
-    n_clusters.append(analysis.n_cluster)
-    hparams_list.append(hparams)
+    #check if performance exceeds target
+    log = tools.load_log(train_dir) 
+    if log['perf_avg'][-1] > hparams['target_perf']: 
+    
+        # variance.compute_variance(train_dir)
+        analysis = clustering.Analysis(train_dir, 'rule')
+        n_clusters.append(analysis.n_cluster)
+        hparams_list.append(hparams)
+    
+        #analysis.plot_example_unit()
+        analysis.plot_variance()
+        analysis.plot_2Dvisualization()
+
 
 plt.figure()
 plt.plot(n_clusters)
+
+
