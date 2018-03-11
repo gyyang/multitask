0e149db68aa49cece28b96f9b985f704670daf19
madhurajoglekar <countess.steph@gmail.com>
2018-03-11 18:43:18 -0400

    more_clustering

--- varyhparams.py
+++ varyhparams.py
@@ -57,28 +57,29 @@ for train_dir in train_dirs:
     if log['perf_min'][-1] > hparams['target_perf']:         
 
             #if hparams['activation']=='tanh': #maddy added - check tanh
-        
-            countval+=1
-            #print countval, train_dir[-2:] #maddy added
-    
-            #print hparams['rnn_type'], log['perf_min'][-1] #maddy added 
-            #if hparams['rnn_type'] == 'LeakyGRU':
-            #    LeakyGRU_perfmin.append(log['perf_min'][-1])
+            if hparams['rnn_type']=='LeakyGRU': #maddy added - check tanh
             
-            # variance.compute_variance(train_dir)
-            analysis = clustering.Analysis(train_dir, 'rule')
-            n_clusters.append(analysis.n_cluster)
-            scores.append(analysis.scores)
-            analysis.plot_cluster_score()
-            hparams_list.append(hparams)
-    
-            log['n_cluster'] = analysis.n_cluster #maddy added save no of clusters. 
-            log['train_dir'] =train_dir
-            #tools.save_log(log)
-                    
-            analysis.plot_example_unit()
-            analysis.plot_variance()
-            analysis.plot_2Dvisualization()
+                countval+=1
+                #print countval, train_dir[-2:] #maddy added
+        
+                #print hparams['rnn_type'], log['perf_min'][-1] #maddy added 
+                #if hparams['rnn_type'] == 'LeakyGRU':
+                #    LeakyGRU_perfmin.append(log['perf_min'][-1])
+                
+                # variance.compute_variance(train_dir)
+                analysis = clustering.Analysis(train_dir, 'rule')
+                n_clusters.append(analysis.n_cluster)
+                scores.append(analysis.scores)
+                analysis.plot_cluster_score()
+                hparams_list.append(hparams)
+        
+                log['n_cluster'] = analysis.n_cluster #maddy added save no of clusters. 
+                log['train_dir'] =train_dir
+                #tools.save_log(log)
+                        
+                analysis.plot_example_unit()
+                analysis.plot_variance()
+                analysis.plot_2Dvisualization()
     
             print "done"
 """
@@ -89,6 +90,11 @@ hparams_list = list()
 initdict = {'diag':[], 'randortho':[],'softplus':[], 'relu':[], 'tanh':[],'LeakyRNN':[], 
             'LeakyGRU':[],'l1_h_0':[], 'l1_weight_0':[],'l1_h_1emin5':[], 'l1_weight_1emin5':[],
             'l1_h_1emin4':[], 'l1_weight_1emin4':[],'l1_h_1emin3':[], 'l1_weight_1emin3':[] }
+
+initdictother = {'LeakyRNNsoftplus':[], 'LeakyRNNrelu':[],'LeakyGRUsoftplus':[], 'LeakyGRUrelu':[]}
+initdictotherother = {'LeakyRNNsoftplusdiag':[], 'LeakyRNNreludiag':[],'LeakyGRUsoftplusdiag':[],
+                      'LeakyGRUreludiag':[],'LeakyRNNsoftplusrandortho':[], 'LeakyRNNrelurandortho':[],
+                      'LeakyGRUsoftplusrandortho':[], 'LeakyGRUrelurandortho':[]}
 
 #actdict = {'softplus':[], 'relu':[], 'tanh':[]}
 #rnndict = {'LeakyRNN':[], 'LeakyGRU':[]}
@@ -105,7 +111,13 @@ for train_dir in train_dirs:
         
         initdict[hparams['w_rec_init']].append(log['n_cluster'])
         initdict[hparams['activation']].append(log['n_cluster'])
-        initdict[hparams['rnn_type']].append(log['n_cluster']) 
+
+        #initdict[hparams['rnn_type']].append(log['n_cluster']) 
+        if hparams['activation'] != 'tanh':#maddy added added to compare leakyrnn/gru without tanh.            
+            initdict[hparams['rnn_type']].append(log['n_cluster']) 
+            initdictother[hparams['rnn_type']+hparams['activation']].append(log['n_cluster']) 
+            initdictotherother[hparams['rnn_type']+hparams['activation']+hparams['w_rec_init']].append(log['n_cluster']) 
+
         if hparams['l1_h'] == 0:
             initdict['l1_h_0'].append(log['n_cluster'])   
         else: #hparams['l1_h'] == 1e-3 or 1e-4 or 1e-5:  
@@ -125,14 +137,48 @@ for train_dir in train_dirs:
 f, axarr = plt.subplots(7, 1,figsize=(3,12),sharex=True)
 u = 0
 for key in initdict.keys():
-    if 'l1_' not in key:
+    if 'l1_' not in key:        
         axarr[u].set_title(key + ' ' + str(len(initdict[key]))+ ' mean: '+str(round(np.mean(initdict[key]),2)) )
         axarr[u].hist(initdict[key])
         u += 1
 f.subplots_adjust(wspace=.3, hspace=0.3)    
 #plt.savefig('./figure/histforcases_96nets.png')
 #plt.savefig('./figure/histforcases__pt9_192nets.pdf')
+#plt.savefig('./figure/histforcases___leakygrunotanh_pt9_192nets.pdf')
 
+f, axarr = plt.subplots(4, 1,figsize=(3,8),sharex=True)
+u = 0
+for key in initdictother.keys():
+    if 'l1_' not in key:        
+        axarr[u].set_title(key + ' ' + str(len(initdictother[key]))+ ' mean: '+str(round(np.mean(initdictother[key]),2)) )
+        axarr[u].hist(initdictother[key])
+        u += 1
+f.subplots_adjust(wspace=.3, hspace=0.3)    
+#plt.savefig('./figure/histforcases__leakyrnngrurelusoftplus_pt9_192nets.pdf')
+
+
+f, axarr = plt.subplots(4, 1,figsize=(3,6),sharex=True)
+u = 0
+for key in initdictotherother.keys():
+    if 'l1_' not in key and 'diag' not in key:        
+        axarr[u].set_title(key + ' ' + str(len(initdictotherother[key]))+ ' mean: '+str(round(np.mean(initdictotherother[key]),2)) )
+        axarr[u].hist(initdictotherother[key])
+        u += 1
+f.subplots_adjust(wspace=.3, hspace=0.3)    
+#plt.savefig('./figure/histforcases_randortho_notanh_pt9_192nets.pdf')
+
+f, axarr = plt.subplots(4, 1,figsize=(3,6),sharex=True)
+u = 0
+for key in initdictotherother.keys():
+    if 'l1_' not in key and 'randortho' not in key:        
+        axarr[u].set_title(key + ' ' + str(len(initdictotherother[key]))+ ' mean: '+str(round(np.mean(initdictotherother[key]),2)) )
+        axarr[u].hist(initdictotherother[key])
+        u += 1
+f.subplots_adjust(wspace=.3, hspace=0.3)    
+#plt.savefig('./figure/histforcases_diag_notanh_pt9_192nets.pdf')
+
+
+#regu--
 f, axarr = plt.subplots(4, 1,figsize=(3,8),sharex=True)
 u = 0
 for key in initdict.keys():
