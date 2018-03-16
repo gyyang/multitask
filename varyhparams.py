d494688d3518893f7629f1df1ee66abe7daf71fd
gyyang <gyyang.neuro@gmail.com>
2018-03-16 16:14:22 -0400

    formatting

--- varyhparams.py
+++ varyhparams.py
@@ -2,6 +2,7 @@
 
 from __future__ import division
 
+from collections import defaultdict
 import os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,7 +12,7 @@ import variance #maddy added
 import clustering
 import standard_analysis #maddy added. 
 
-DATAPATH = os.path.join(os.getcwd(), 'data', 'debug')
+DATAPATH = os.path.join(os.getcwd(), 'data', 'train_varyhparams')
 
 # Get all the subdirectories
 train_dirs = [os.path.join(DATAPATH, d) for d in os.listdir(DATAPATH)]
@@ -87,33 +88,35 @@ for train_dir in train_dirs:
 #maddy added -- check no of clusters under various conditions. 
 hparams_list = list()
 #initdict = {'diag':[], 'randortho':[],'softplus':[], 'relu':[], 'tanh':[],'LeakyRNN':[], 'LeakyGRU':[]}
-initdict = {'diag':[], 'randortho':[],'softplus':[], 'relu':[], 'tanh':[],'LeakyRNN':[], 
-            'LeakyGRU':[],'l1_h_0':[], 'l1_weight_0':[],'l1_h_1emin5':[], 'l1_weight_1emin5':[],
-            'l1_h_1emin4':[], 'l1_weight_1emin4':[],'l1_h_1emin3':[], 'l1_weight_1emin3':[] }
-
-initdictother = {'LeakyRNNsoftplus':[], 'LeakyRNNrelu':[],'LeakyGRUsoftplus':[], 'LeakyGRUrelu':[]}
-initdictotherother = {'LeakyRNNsoftplusdiag':[], 'LeakyRNNreludiag':[],'LeakyGRUsoftplusdiag':[],
-                      'LeakyGRUreludiag':[],'LeakyRNNsoftplusrandortho':[], 'LeakyRNNrelurandortho':[],
-                      'LeakyGRUsoftplusrandortho':[], 'LeakyGRUrelurandortho':[]}
+# initdict = {'diag':[], 'randortho':[],'softplus':[], 'relu':[], 'tanh':[],'LeakyRNN':[],
+#             'LeakyGRU':[],'l1_h_0':[], 'l1_weight_0':[],'l1_h_1emin5':[], 'l1_weight_1emin5':[],
+#             'l1_h_1emin4':[], 'l1_weight_1emin4':[],'l1_h_1emin3':[], 'l1_weight_1emin3':[] }
+#
+# initdictother = {'LeakyRNNsoftplus':[], 'LeakyRNNrelu':[],'LeakyGRUsoftplus':[], 'LeakyGRUrelu':[]}
+# initdictotherother = {'LeakyRNNsoftplusdiag':[], 'LeakyRNNreludiag':[],'LeakyGRUsoftplusdiag':[],
+#                       'LeakyGRUreludiag':[],'LeakyRNNsoftplusrandortho':[], 'LeakyRNNrelurandortho':[],
+#                       'LeakyGRUsoftplusrandortho':[], 'LeakyGRUrelurandortho':[]}
+initdict = defaultdict(list)
+initdictother = defaultdict(list)
+initdictotherother = defaultdict(list)
 
 #actdict = {'softplus':[], 'relu':[], 'tanh':[]}
 #rnndict = {'LeakyRNN':[], 'LeakyGRU':[]}
 
 for train_dir in train_dirs:
-    
     hparams = tools.load_hparams(train_dir)
     #check if performance exceeds target
     log = tools.load_log(train_dir) 
     #if log['perf_avg'][-1] > hparams['target_perf']: 
     if log['perf_min'][-1] > hparams['target_perf']:         
-        print "no. of clusters ", log['n_cluster']
+        print('no. of clusters', log['n_cluster'])
         n_clusters.append(log['n_cluster'])
         
         initdict[hparams['w_rec_init']].append(log['n_cluster'])
         initdict[hparams['activation']].append(log['n_cluster'])
 
         #initdict[hparams['rnn_type']].append(log['n_cluster']) 
-        if hparams['activation'] != 'tanh':#maddy added added to compare leakyrnn/gru without tanh.            
+        if hparams['activation'] != 'tanh':
             initdict[hparams['rnn_type']].append(log['n_cluster']) 
             initdictother[hparams['rnn_type']+hparams['activation']].append(log['n_cluster']) 
             initdictotherother[hparams['rnn_type']+hparams['activation']+hparams['w_rec_init']].append(log['n_cluster']) 
@@ -132,84 +135,83 @@ for train_dir in train_dirs:
             
         #initdict[hparams['l1_weight']].append(log['n_cluster'])      
         
+def plot_histogram():
+    # Check no of clusters under various conditions.
+    f, axarr = plt.subplots(7, 1, figsize=(3,12), sharex=True)
+    u = 0
+    for key in initdict.keys():
+        if 'l1_' not in key:
+            title = (key + ' ' + str(len(initdict[key])) +
+                     ' mean: '+str(round(np.mean(initdict[key]),2)))
+            axarr[u].set_title(title)
+            axarr[u].hist(initdict[key])
+            u += 1
+    f.subplots_adjust(wspace=.3, hspace=0.3)
+    # plt.savefig('./figure/histforcases_96nets.png')
+    # plt.savefig('./figure/histforcases__pt9_192nets.pdf')
+    # plt.savefig('./figure/histforcases___leakygrunotanh_pt9_192nets.pdf')
 
- #maddy added -- check no of clusters under various conditions.    
-f, axarr = plt.subplots(7, 1,figsize=(3,12),sharex=True)
-u = 0
-for key in initdict.keys():
-    if 'l1_' not in key:        
-        axarr[u].set_title(key + ' ' + str(len(initdict[key]))+ ' mean: '+str(round(np.mean(initdict[key]),2)) )
-        axarr[u].hist(initdict[key])
-        u += 1
-f.subplots_adjust(wspace=.3, hspace=0.3)    
-#plt.savefig('./figure/histforcases_96nets.png')
-#plt.savefig('./figure/histforcases__pt9_192nets.pdf')
-#plt.savefig('./figure/histforcases___leakygrunotanh_pt9_192nets.pdf')
-
-f, axarr = plt.subplots(4, 1,figsize=(3,8),sharex=True)
-u = 0
-for key in initdictother.keys():
-    if 'l1_' not in key:        
-        axarr[u].set_title(key + ' ' + str(len(initdictother[key]))+ ' mean: '+str(round(np.mean(initdictother[key]),2)) )
-        axarr[u].hist(initdictother[key])
-        u += 1
-f.subplots_adjust(wspace=.3, hspace=0.3)    
-#plt.savefig('./figure/histforcases__leakyrnngrurelusoftplus_pt9_192nets.pdf')
-
-
-f, axarr = plt.subplots(4, 1,figsize=(3,6),sharex=True)
-u = 0
-for key in initdictotherother.keys():
-    if 'l1_' not in key and 'diag' not in key:        
-        axarr[u].set_title(key + ' ' + str(len(initdictotherother[key]))+ ' mean: '+str(round(np.mean(initdictotherother[key]),2)) )
-        axarr[u].hist(initdictotherother[key])
-        u += 1
-f.subplots_adjust(wspace=.3, hspace=0.3)    
-#plt.savefig('./figure/histforcases_randortho_notanh_pt9_192nets.pdf')
-
-f, axarr = plt.subplots(4, 1,figsize=(3,6),sharex=True)
-u = 0
-for key in initdictotherother.keys():
-    if 'l1_' not in key and 'randortho' not in key:        
-        axarr[u].set_title(key + ' ' + str(len(initdictotherother[key]))+ ' mean: '+str(round(np.mean(initdictotherother[key]),2)) )
-        axarr[u].hist(initdictotherother[key])
-        u += 1
-f.subplots_adjust(wspace=.3, hspace=0.3)    
-#plt.savefig('./figure/histforcases_diag_notanh_pt9_192nets.pdf')
+    f, axarr = plt.subplots(4, 1, figsize=(3,8), sharex=True)
+    u = 0
+    for key in initdictother.keys():
+        if 'l1_' not in key:
+            axarr[u].set_title(key + ' ' + str(len(initdictother[key]))+ ' mean: '+str(round(np.mean(initdictother[key]),2)) )
+            axarr[u].hist(initdictother[key])
+            u += 1
+    f.subplots_adjust(wspace=.3, hspace=0.3)
+    # plt.savefig('./figure/histforcases__leakyrnngrurelusoftplus_pt9_192nets.pdf')
 
 
-#regu--
-f, axarr = plt.subplots(4, 1,figsize=(3,8),sharex=True)
-u = 0
-for key in initdict.keys():
-    if 'l1_h_' in key:
-        axarr[u].set_title(key + ' ' + str(len(initdict[key]))+ ' mean: '+str(round(np.mean(initdict[key]),2)) )
-        axarr[u].hist(initdict[key])
-        u += 1
-f.subplots_adjust(wspace=.3, hspace=0.3)    
-#plt.savefig('./figure/noofclusters_pt9_l1_h_192nets.pdf')
+    f, axarr = plt.subplots(4, 1, figsize=(3,6), sharex=True)
+    u = 0
+    for key in initdictotherother.keys():
+        if 'l1_' not in key and 'diag' not in key:
+            axarr[u].set_title(key + ' ' + str(len(initdictotherother[key]))+ ' mean: '+str(round(np.mean(initdictotherother[key]),2)) )
+            axarr[u].hist(initdictotherother[key])
+            u += 1
+    f.subplots_adjust(wspace=.3, hspace=0.3)
+    # plt.savefig('./figure/histforcases_randortho_notanh_pt9_192nets.pdf')
 
-f, axarr = plt.subplots(4, 1,figsize=(3,8),sharex=True)
-u = 0
-for key in initdict.keys():
-    if 'l1_weight_' in key:
-        axarr[u].set_title(key + ' ' + str(len(initdict[key])) + ' mean: '+str(round(np.mean(initdict[key]),2)) )
-        axarr[u].hist(initdict[key])
-        u += 1
-f.subplots_adjust(wspace=.3, hspace=0.3)    
-#plt.savefig('./figure/noofclusters_pt9_l1_weight_192nets.pdf')
+    f, axarr = plt.subplots(4, 1, figsize=(3,6),sharex=True)
+    u = 0
+    for key in initdictotherother.keys():
+        if 'l1_' not in key and 'randortho' not in key:
+            axarr[u].set_title(key + ' ' + str(len(initdictotherother[key]))+ ' mean: '+str(round(np.mean(initdictotherother[key]),2)) )
+            axarr[u].hist(initdictotherother[key])
+            u += 1
+    f.subplots_adjust(wspace=.3, hspace=0.3)
+    # plt.savefig('./figure/histforcases_diag_notanh_pt9_192nets.pdf')
 
 
-#maddy added -- plot no of clusters         
-#"""
-plt.figure()
-plt.plot(n_clusters,'.-')
-plt.xlabel('networks')
-plt.ylabel('no. of clusters')
-plt.title('target perf-min 0.9, total:'+str(len(n_clusters))) #
-#plt.savefig('./figure/noofclusters_pt9_192nets.pdf')
+    #regu--
+    f, axarr = plt.subplots(4, 1,figsize=(3,8),sharex=True)
+    u = 0
+    for key in initdict.keys():
+        if 'l1_h_' in key:
+            axarr[u].set_title(key + ' ' + str(len(initdict[key]))+ ' mean: '+str(round(np.mean(initdict[key]),2)) )
+            axarr[u].hist(initdict[key])
+            u += 1
+    f.subplots_adjust(wspace=.3, hspace=0.3)
+    #plt.savefig('./figure/noofclusters_pt9_l1_h_192nets.pdf')
 
-#"""
-#plt.savefig('./figure/noofclusters_pt9_96nets.pdf')
-#min score 90% -- pick cluster with max sil score. 
+    f, axarr = plt.subplots(4, 1,figsize=(3,8),sharex=True)
+    u = 0
+    for key in initdict.keys():
+        if 'l1_weight_' in key:
+            axarr[u].set_title(key + ' ' + str(len(initdict[key])) + ' mean: '+str(round(np.mean(initdict[key]),2)) )
+            axarr[u].hist(initdict[key])
+            u += 1
+    f.subplots_adjust(wspace=.3, hspace=0.3)
+    #plt.savefig('./figure/noofclusters_pt9_l1_weight_192nets.pdf')
 
+
+def plot_n_clusters():
+    """Plot the number of clusters."""
+    plt.figure()
+    plt.plot(n_clusters,'.-')
+    plt.xlabel('networks')
+    plt.ylabel('no. of clusters')
+    plt.title('target perf-min 0.9, total:'+str(len(n_clusters))) #
+    #plt.savefig('./figure/noofclusters_pt9_192nets.pdf')
+
+plot_n_clusters()
