5b6c972184327a5dfe13b1912cc330927ae67842
gyyang <gyyang.neuro@gmail.com>
2018-03-29 20:37:10 -0400

    add a new test case

--- network_test.py
+++ network_test.py
@@ -11,18 +11,23 @@ import network
 class NetworkTest(unittest.TestCase):
 
   def testTFPopVec(self):
-    batch_size = 15
-    n_units = 36
-    y = np.random.rand(batch_size, n_units)
-    theta = network.popvec(y)
+      n_units = 36
+      batch_size = n_units
+      ys = [
+          np.random.rand(batch_size, n_units),
+          np.eye(batch_size, n_units)
+      ]
 
-    y2 = tf.placeholder('float', [batch_size, n_units])
-    theta2 = network.tf_popvec(y2)
-    with tf.Session() as sess:
-      theta2_val = sess.run(theta2, feed_dict={y2: y})
+      for y in ys:
+          theta = network.popvec(y)
 
-    self.assertTrue(np.allclose(theta, theta2_val))
+          y2 = tf.placeholder('float', [batch_size, n_units])
+          theta2 = network.tf_popvec(y2)
+          with tf.Session() as sess:
+              theta2_val = sess.run(theta2, feed_dict={y2: y})
+          print(theta2_val)
+          self.assertTrue(np.allclose(theta, theta2_val))
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
