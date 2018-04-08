460912226122f1207eedd35205f4695bf21314be
gyyang <gyyang.neuro@gmail.com>
2018-04-08 14:31:51 -0400

    compute rate from spikes

--- siegel_dataset_preprocess.py
+++ siegel_dataset_preprocess.py
@@ -61,6 +61,48 @@ def _load_tables(fname, table_name):
     return table_dict
 
 
+def _get_valid_trials(trial_infos):
+    """Get valid trials.
+
+    Args:
+        trial_infos: dict of arrays.
+
+    Returns:
+        new_trial_infos: dict of arrays, only contain valid trials
+        valid_trials: list of bools, indicating the valid trials
+    """
+    new_trial_infos = defaultdict(list)
+    keys = trial_infos.keys()
+    n_trial = len(trial_infos[keys[0]])
+    valid_trials = list()
+    for i_trial in range(n_trial):
+        if (trial_infos['badTrials'][i_trial] == 1 or
+            math.isnan(trial_infos['responseTime'][i_trial]) or
+            trial_infos['responseTime'][i_trial] < 0.2
+        ):
+            valid = False
+        else:
+            valid = True
+        valid_trials.append(valid)
+        if valid:
+            for key in keys:
+                new_trial_infos[key].append(trial_infos[key][i_trial])
+    return new_trial_infos, valid_trials
+
+
+# def _spike_to_rate(spikes_unit, times):
+#     """Convert spikes to rate.
+#
+#     Args:
+#         spikes_unit: list of float, a list of spike times
+#         times: list of float, a list of time points, default unit second
+#
+#     Returns:
+#         rates: list of float, a list of rate, default unit spike/second, Hz
+#             rates will be the same size as times
+#     """
+
+
 if __name__ == '__main__':
     # rate1s, rate2s = get_mante_data()
 
@@ -72,48 +114,27 @@ if __name__ == '__main__':
     f = files[0]
 
     trial_infos = _load_tables(f, 'trialinfo')
+    trial_infos, valid_trials = _get_valid_trials(trial_infos)
     unit_infos = _load_tables(f, 'unitinfo')
     electrode_infos = _load_tables(f, 'electrodeinfo')
 
     spikes = _load_spikes(f)
+    spikes = spikes[valid_trials, :]
+
     n_trial, n_unit = spikes.shape
 
     assert len(trial_infos.values()[0]) == n_trial
     assert len(unit_infos.values()[0]) == n_unit
 
-    # Get valid trials
-    new_trial_infos = defaultdict(list)
-    keys = trial_infos.keys()
-    valid_trials = list()
-    for i_trial in range(n_trial):
-        if (trial_infos['badTrials'][i_trial] == 1 or
-            math.isnan(trial_infos['responseTime'][i_trial])):
-            valid = False
-        else:
-            valid = True
-        valid_trials.append(valid)
-        if valid:
-            for key in keys:
-                new_trial_infos[key].append(trial_infos[key][i_trial])
+    bin_size = 0.05  # unit: second
+    bins = np.arange(-0.05, 0.2, bin_size)
+    n_time = len(bins) - 1
+    rates = np.zeros((n_trial, n_time, n_unit))
 
-    # # For each trial and each neuron, convert the spike times into a rate
-    # n_time = 100
-    # Rates = np.zeros((n_unit, n_trial, n_time))  # (-2.5s, 3.5s)
-    # mins = list()
-    # maxs = list()
-    # for i_trial in range(n_trial):
-    #     for i_neuron in range(n_unit):
-    #         # Convert the spikes into a rate
-    #         spikes_tmp = spikes[i_trial, i_neuron]
-    #         try:
-    #             mins.append(np.min(spikes_tmp))
-    #             maxs.append(np.max(spikes_tmp))
-    #         except ValueError:
-    #             pass
-    #         # rate = convert_to_rate(spikes_tmp)
-    #         # Rates[i_trial, i_neuron] = rate
-    i_trial = 2
-    i_unit = 0
-    spikes_unit = spikes[i_trial, i_unit]
-    # trial_info = trial_infos[i_trial]
-    # unit_info = unit_infos[i_unit]
+    for i_unit in range(n_unit):
+        for i_trial in range(n_trial):
+            spikes_unit = spikes[i_trial, i_unit]
+            # Compute PSTH
+            hist, bin_edges = np.histogram(spikes_unit, bins=bins)
+            rate = hist/bin_size
+            rates[i_trial, :, i_unit] = rate
